a115af0f85aae19de1430b85a8450701f1747f8f
Don <chongdon2000@gmail.com>
2025-08-07 23:36:27 -0600

    TOC highlight draft

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -1,12 +1,51 @@
+import { useRef } from 'react';
+import { motion, AnimatePresence, useIsPresent } from 'framer-motion';
 import { useSectionStore } from '@/components/SectionProvider';
+import { remToPx } from '@/lib/remToPx';
+import { useIsInsideMobileNavigation } from '@/components/MobileNavigation';
+
+function useInitialValue<T>(value: T, condition = true) {
+  let initialValue = useRef(value).current
+  return condition ? initialValue : value
+}
+
 export function TableOfContents() {
   // console.log(useSectionStore((s) => s.sections));
 
   const tableOfContents = useSectionStore((s) => s.sections);
+  let isInsideMobileNavigation = useIsInsideMobileNavigation();
+  let [sections, visibleSections] = useInitialValue(
+    [useSectionStore((s) => s.sections), useSectionStore((s) => s.visibleSections)],
+    isInsideMobileNavigation,
+  );
+
+  let firstVisibleSectionIndex = Math.max(
+    0,
+    [{ id: '_top' }, ...sections].findIndex(
+      (section) => section.id === visibleSections[0],
+    ),
+  );
+  let itemHeight = remToPx(2);
+  let height = useIsPresent()
+    ? Math.max(1, visibleSections.length) * itemHeight
+    : itemHeight;
+  let top = firstVisibleSectionIndex * itemHeight;
 
   return (
     <div className="hidden xl:block xl:w-64 xl:flex-none xl:sticky xl:top-[4.75rem] xl:h-[calc(100vh-4.75rem)] xl:overflow-y-auto xl:py-16 xl:pl-6 xl:border-l xl:border-zinc-900/10 xl:dark:border-white/10">
-      <nav aria-labelledby="on-this-page-title">
+      <nav aria-labelledby="on-this-page-title" className="relative pl-2">
+        <AnimatePresence initial={!isInsideMobileNavigation}>
+          {sections.length > 0 && (
+            <motion.div
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.2 } }}
+              exit={{ opacity: 0 }}
+              className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
+              style={{ borderRadius: 8, height, top }}
+            />
+          )}
+        </AnimatePresence>
         {tableOfContents.length > 0 && (
           <>
             <h2
@@ -17,10 +56,11 @@ export function TableOfContents() {
             </h2>
             <ol role="list" className="mt-4 space-y-3 text-sm">
               {tableOfContents.map((section) => (
-                <li key={section.id}>
+                <li key={section.id} className="relative">
                   <h3>
                     <a
                       href={`#${section.id}`}
+                      className="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
                     >
                       {section.title}
                     </a>
