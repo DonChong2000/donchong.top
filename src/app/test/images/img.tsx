02986f8a38a9b92190fa544f13afce7ca1c4d35a
Don <chongdon2000@gmail.com>
2025-10-06 23:00:40 -0600

    Quick fix on build problem
    npm i -D @types/webpack-env

--- src/app/test/images/img.tsx
+++
@@ -1,6 +0,0 @@
-const imageContext = require.context('', false, /\.(png|jpe?g|svg|gif)$/);
-export default imageContext.keys().reduce((acc, path) => {
-  const key = path.replace('./', '');
-  acc[key] = imageContext(path);
-  return acc;
-}, {});
