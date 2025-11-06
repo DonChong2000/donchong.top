7ca4b8e89e29ce90bd486935928032456048f931
Don <chongdon2000@gmail.com>
2025-11-06 14:40:40 -0700

    Overview Layout done

--- src/components/blogSection.tsx
+++ src/components/blogSection.tsx
@@ -1,24 +1,26 @@
+import Image from 'next/image';
+
 const posts = [
   {
     id: 1,
-    title: 'Boost your conversion rate',
+    title: 'Swimming Pool Drown Detection and Rescue System (FYP)',
     href: '#',
     description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+      'A system that detects drowning incidents using a wearable device, pinpoints the location with computer vision, and deploys a robotic float for active rescue. I contributed to the concept, research, WebApp, and integration of the computation, wearable, and robotic components, as well as assisting in wearable development.',
     imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+      '/homepage/fyp.jpg',
     date: 'Mar 16, 2020',
     datetime: '2020-03-16',
     category: { title: 'Marketing', href: '#' }, 
   },
   {
-    id: 1,
+    id: 2,
     title: 'Boost your conversion rate',
     href: '#',
     description:
       'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
     imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+      '/homepage/fyp.jpg',
     date: 'Mar 16, 2020',
     datetime: '2020-03-16',
     category: { title: 'Marketing', href: '#' },
@@ -34,33 +36,34 @@ export default function BlogSection() {
             {posts.map((post) => (
               <article key={post.id} className="relative isolate flex flex-col gap-8 lg:flex-row">
                 <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-                  <img
+                  <Image
                     alt=""
                     src={post.imageUrl}
+                    fill
                     className="absolute inset-0 h-full w-full rounded-2xl object-cover"
                   />
-                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-charcoal-900/10 dark:ring-white/10" />
                 </div>
                 <div>
                   <div className="flex items-center gap-x-4 text-xs">
-                    <time dateTime={post.datetime} className="text-gray-500">
+                    <time dateTime={post.datetime} className="text-gray-500 dark:text-gray-400">
                       {post.date}
                     </time>
                     <a
                       href={post.category.href}
-                      className="relative z-10 rounded-full bg-timberwolf-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                      className="relative z-10 rounded-full px-3 py-1.5 font-medium bg-timberwolf-50 text-charcoal hover:bg-timberwolf-100 dark:hover:bg-timberwolf-200"
                     >
                       {post.category.title}
                     </a>
                   </div>
                   <div className="group relative max-w-xl">
-                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-charcoal-460 group-hover:text-charcoal-600 dark:text-timberwolf-400 dark:group-hover:text-timberwolf-100">
                       <a href={post.href}>
                         <span className="absolute inset-0" />
                         {post.title}
                       </a>
                     </h3>
-                    <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
+                    <p className="mt-5 text-sm leading-6 text-charcoal-700 dark:text-charcoal-100">{post.description}</p>
                   </div>
                 </div>
               </article>
