7292050a55878e50757a4b78289c97c56cb1d535
Don <chongdon2000@gmail.com>
2025-11-05 21:15:25 -0700

    Herosection for project overview

--- src/components/HeroPattern.tsx
+++ src/components/HeroPattern.tsx
@@ -1,22 +1,16 @@
-import { GridPattern } from '@/components/GridPattern';
+import { GraphPattern } from '@/components/GraphPattern';
 
 export function HeroPattern() {
   return (
     <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
       <div className="absolute top-0 left-1/2 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
-        <div className="absolute inset-0 bg-linear-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100">
-          <GridPattern
+        <div className="absolute inset-0 bg-linear-to-br from-[#91a1c4] to-[#1c1665] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#]/30 dark:to-[#FFFFFF]/30 dark:opacity-100">
+          <GraphPattern
             width={72}
             height={56}
             x={-12}
             y={4}
-            squares={[
-              [4, 3],
-              [2, 1],
-              [7, 3],
-              [10, 6],
-            ]}
-            className="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-18deg] fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5"
+            className="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-5deg] fill-black/5 stroke-black/5 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/15"
           />
         </div>
         <svg
