9aa0404369953b64cc26e7455511e08e173afe68
Don <chongdon2000@gmail.com>
2026-04-14 00:30:04 -0600

    fix(ui): DetailOverlay is not always square

--- src/components/DetailOverlay.tsx
+++ src/components/DetailOverlay.tsx
@@ -21,7 +21,7 @@ export function DetailOverlay({ callouts }: DetailOverlayProps) {
           <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 mt-4">
             {callouts.map((callout) => (
               <div key={callout.name} className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg  sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64 GH not-prose">
+                <div className="relative w-full overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-2 lg:aspect-square group-hover:opacity-75 GH not-prose">
                   <Image
                     src={callout.imageSrc}
                     alt={callout.imageAlt}
