b6b30822cb1cb6d730d008cb3c3ce985bac16eb2
Don <chongdon2000@gmail.com>
2026-04-14 00:30:29 -0600

    chore: Formatting DetailOverlay.tsx

--- src/components/DetailOverlay.tsx
+++ src/components/DetailOverlay.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 interface Callout {
   name: string;
@@ -15,32 +15,30 @@ interface DetailOverlayProps {
 
 export function DetailOverlay({ callouts }: DetailOverlayProps) {
   return (
-
-      <div>
-
-          <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 mt-4">
-            {callouts.map((callout) => (
-              <div key={callout.name} className="group relative">
-                <div className="relative w-full overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-2 lg:aspect-square group-hover:opacity-75 GH not-prose">
-                  <Image
-                    src={callout.imageSrc}
-                    alt={callout.imageAlt}
-                    fill
-                    priority={callout.priority}
-                    sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <h3 className="mt-6 text-sm">
-                  <a href={callout.href}>
-                    <span className="absolute inset-0" />
-                    {callout.name}
-                  </a>
-                </h3>
-                <p className="text-base font-semibold">{callout.description}</p>
-              </div>
-            ))}
+    <div>
+      <div className="mt-4 space-y-12 lg:grid lg:grid-cols-3 lg:space-y-0 lg:gap-x-6">
+        {callouts.map((callout) => (
+          <div key={callout.name} className="group relative">
+            <div className="GH not-prose relative w-full overflow-hidden rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-square">
+              <Image
+                src={callout.imageSrc}
+                alt={callout.imageAlt}
+                fill
+                priority={callout.priority}
+                sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <h3 className="mt-6 text-sm">
+              <a href={callout.href}>
+                <span className="absolute inset-0" />
+                {callout.name}
+              </a>
+            </h3>
+            <p className="text-base font-semibold">{callout.description}</p>
           </div>
-        </div>
+        ))}
+      </div>
+    </div>
   );
 }
