23496d79e1116c700912887e6bb8e7623b974c4a
Don <chongdon2000@gmail.com>
2026-04-25 16:29:55 -0600

    feat(ui): add back link and nav for GTNH pages

---
+++ src/app/hobbies/gtnh/tierCategories.ts
@@ -0,0 +1,24 @@
+import type { ScrollingCardCategory } from '@/components/ScrollingCards';
+
+export const tierCategories: ScrollingCardCategory[] = [
+  {
+    name: 'ULV - MV',
+    href: '/hobbies/gtnh/ULV-MV',
+    imageSrc: '/2025-08-10_22.17.15.png',
+  },
+  {
+    name: 'HV - EV',
+    href: '/hobbies/gtnh/HV-EV',
+    imageSrc: '/2025-08-10_22.41.03.png',
+  },
+  {
+    name: 'IV',
+    href: '/hobbies/gtnh/IV-tier',
+    imageSrc: '/2025-08-10_23.56.43.png',
+  },
+  {
+    name: 'LUV',
+    href: '/hobbies/gtnh#luv-tier',
+    imageSrc: '/Pasted%20image%2020250630155824.png',
+  },
+];
